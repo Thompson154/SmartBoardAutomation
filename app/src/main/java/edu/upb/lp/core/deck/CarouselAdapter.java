0f8b15d21af7dee440af48d2f171fb907f9ce042
AnderMichael <andercaymi@gmail.com>
2024-12-12 21:19:14 -0400

    chore: Adding view holder factory to create cards

--- app/src/main/java/edu/upb/lp/core/deck/CarouselAdapter.java
+++ app/src/main/java/edu/upb/lp/core/deck/CarouselAdapter.java
@@ -3,7 +3,6 @@ package edu.upb.lp.core.deck;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -18,45 +17,56 @@ import java.util.List;
 
 import edu.upb.lp.genericgame.R;
 
-public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHolder> {
+public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.CardViewHolder> {
+    public static final int TYPE_CARD_SCREEN = 0;
+    public static final int TYPE_CARD_START = 1;
 
     private final Context context;
-    private final List<Card> cards;
+    private List<Card> cards;
 
     public CarouselAdapter(Context context, List<Card> cards) {
         this.context = context;
         this.cards = cards;
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position != cards.size() - 1) {
+            return TYPE_CARD_SCREEN;
+        } else {
+            return TYPE_CARD_START;
+        }
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return (CardViewHolder) CarouselViewHolderFactory.create(parent, viewType, context);
+    }
+
     // TODO: Util o Helper?
     private int getSourceId(Activity activity, String sourceName) {
         @SuppressLint("DiscouragedApi") int id = activity.getResources().getIdentifier(sourceName, "drawable", activity.getPackageName());
         return id;
     }
 
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.screen, parent, false);
-        return new ViewHolder(view);
-    }
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+        if (getItemViewType(position) == TYPE_CARD_SCREEN) {
+            CardScreenCardViewHolder cardHolder = (CardScreenCardViewHolder) holder;
+            CardScreen cardScreen = (CardScreen) cards.get(position);
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Card card = cards.get(position);
-        if (position != cards.size() - 1) {
-            CardScreen cardScreen = (CardScreen) card;
-            holder.title.setText(cardScreen.getTitle());
-            holder.description.setText(cardScreen.getDescription());
-            int idImageSource = getSourceId((Activity) holder.itemView.getContext(), cardScreen.getImage());
-            holder.image.setImageResource(idImageSource);
-            holder.button.setVisibility(View.GONE);
+            cardHolder.title.setText(cardScreen.getTitle());
+            cardHolder.description.setText(cardScreen.getDescription());
+            int idImageSource = getSourceId((Activity) cardHolder.itemView.getContext(), cardScreen.getImage());
+            cardHolder.image.setImageResource(idImageSource);
         } else {
-            CardStart cardStart = (CardStart) card;
-            holder.description.setText("");
-            holder.button.setText(cardStart.getButtonText());
-            holder.button.setVisibility(View.VISIBLE);
-            holder.button.setOnClickListener(v -> {
-                ((Activity) holder.itemView.getContext()).finish();
+            CardStartCardViewHolder startHolder = (CardStartCardViewHolder) holder;
+            CardStart cardStart = (CardStart) cards.get(position);
+
+            startHolder.button.setText(cardStart.getButtonText());
+            startHolder.button.setOnClickListener(v -> {
+                ((Activity) startHolder.itemView.getContext()).finish();
             });
         }
     }
@@ -66,17 +76,30 @@ public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHo
         return cards.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    public static class CardScreenCardViewHolder extends CardViewHolder {
         TextView title, description;
         ImageView image;
+
+        public CardScreenCardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title_text);
+            description = itemView.findViewById(R.id.description_text);
+            image = itemView.findViewById(R.id.center_image);
+        }
+    }
+
+    public static class CardStartCardViewHolder extends CardViewHolder {
         Button button;
 
-        public ViewHolder(@NonNull View itemView) {
+        public CardStartCardViewHolder(@NonNull View itemView) {
             super(itemView);
-            title = itemView.findViewById(R.id.screenTitle);
-            description = itemView.findViewById(R.id.screenDescription);
-            image = itemView.findViewById(R.id.screenImage);
-            button = itemView.findViewById(R.id.screenButton);
+            button = itemView.findViewById(R.id.btn_start);
         }
     }
 }
