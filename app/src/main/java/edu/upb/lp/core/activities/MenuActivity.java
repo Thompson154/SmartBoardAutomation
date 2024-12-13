cc44ba81a0d9edab5d2cd00cf22b434f5743fd4c
marianZamorano <mzamorano295@gmail.com>
2024-12-13 17:01:09 -0400

    Add Mas sobre nosotros activity

--- app/src/main/java/edu/upb/lp/core/activities/MenuActivity.java
+++ app/src/main/java/edu/upb/lp/core/activities/MenuActivity.java
@@ -2,7 +2,6 @@ package edu.upb.lp.core.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
